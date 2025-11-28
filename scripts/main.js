3fe6a936f420298fb81f6ebc2e730d22855a3370
scoobymr351-tech <scoobymr351@gmail.com>
2025-11-28 09:00:34 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+document.getElementById('btn1').addEventListener('click', function() {
+    window.location.href = 'yes-page.html';
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+    window.location.href = 'no-page.html';
+});
